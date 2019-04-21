287ff9a4af0afaf1b568dd2c985e3dd18df5504b
luviarag <luvivianaraujo@gmail.com>
2019-04-21 18:47:45 -0500

    commit

--- src/config/config.js
+++ src/config/config.js
@@ -6,6 +6,6 @@ if (process.env.NODE_ENV === 'local'){
 	urlDB = 'mongodb://localhost:27017/plataformavirtual';
 }
 else {
-	urlDB = 'mongodb+srv://luvivianaraujo:luviarag4413%2f@clusterplataformavirtual-ix6si.mongodb.net/plataformavirtual?retryWrites=true'
+	urlDB = 'mongodb+srv://luvivianaraujo:luviarag4413%2f%2a@clusterplataformavirtual-ix6si.mongodb.net/plataformavirtual?retryWrites=true'
 }
 process.env.URLDB = urlDB
